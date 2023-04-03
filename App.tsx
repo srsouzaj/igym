b2c6071d0c55097e9c83421c654d5d8a539d3cfd
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 19:27:48 -0300

    feat: add routes

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { theme } from './src/theme';
 
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
