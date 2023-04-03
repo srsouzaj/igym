8f0e4cff19dcfa6657f7bcbac8fd005d34e360b7
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 18:52:39 -0300

    fix: loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,17 @@
-import { Center, Spinner } from 'native-base';
+import { Center, Image, Spinner, Text, VStack } from 'native-base';
+import LogoSvg from '@assets/logo.svg';
 
 export function Loading() {
     return (
         <Center flex={1} bg="gray.700">
+            <VStack p="16px" alignItems="center">
+                <LogoSvg />
+                <Text py='8px' color="gray.100" fontSize="sm">
+                    Treine sua mente e o seu corpo.
+                </Text>
+            </VStack>
             <Spinner color="green.500" />
+
         </Center>
     );
 }
