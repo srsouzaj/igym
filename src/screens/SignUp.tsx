12138dc74bedad513924ad40efcc75323474758c
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 19:40:41 -0300

    feat: add authenticated navigation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,11 +1,25 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
 export function SignUp() {
+
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
     return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
+        >
             <VStack flex={1} px={10} pb={16}>
                 <Image
                     source={BackgroundImg}
@@ -14,28 +28,35 @@ export function SignUp() {
                     resizeMode="contain"
                     position="absolute"
                 />
+
                 <Center my={24}>
                     <LogoSvg />
+
                     <Text color="gray.100" fontSize="sm">
                         Treine sua mente e o seu corpo.
                     </Text>
                 </Center>
+
                 <Center>
                     <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
                         Crie sua conta
                     </Heading>
+
                     <Input
                         placeholder="Nome"
                     />
+
                     <Input
                         placeholder="E-mail"
                         keyboardType="email-address"
                         autoCapitalize="none"
+
                     />
                     <Input
                         placeholder="Senha"
                         secureTextEntry
                     />
+
                     <Button title="Criar e acessar" />
                 </Center>
 
@@ -43,6 +64,7 @@ export function SignUp() {
                     title="Voltar para o login"
                     variant="outline"
                     mt={24}
+                    onPress={handleGoBack}
                 />
             </VStack>
         </ScrollView>
