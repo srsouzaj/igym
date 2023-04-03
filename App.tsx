40e455aa05eb06efb329a27d87bb1f97f144d31a
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 19:33:12 -0300

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -2,22 +2,23 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+
 import { theme } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={theme}>
+
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
