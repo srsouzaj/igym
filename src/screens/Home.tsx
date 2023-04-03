06c8e045d22e327632438c64944ad7a3e8b30ac5
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 20:05:51 -0300

    feat: event pressed styles

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { Text, VStack } from 'native-base';
+import { HStack, Text, VStack } from 'native-base';
 
 export function Home() {
     return (
         <VStack flex={1}>
             <HomeHeader />
-            <Group name="Costas" />
+            <HStack>
+                <Group name="Costa" />
+                <Group name="ombro" />
+            </HStack>
         </VStack>
     );
 }
