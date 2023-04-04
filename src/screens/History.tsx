f1cdf1384d9cfb0afddaabdfd57e499f6ae821b5
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-04 17:47:55 -0300

    feat: listEmpty styled

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-
 export function History() {
     const [exercises, setExercises] = useState([
         {
@@ -15,11 +14,9 @@ export function History() {
             data: ["Puxada frontal"]
         }
     ]);
-
     return (
         <VStack flex={1}>
             <ScreenHeader title='Histórico' />
-
             <SectionList
                 sections={exercises}
                 keyExtractor={item => item}
@@ -32,6 +29,13 @@ export function History() {
                     </Heading>
                 )}
                 px={8}
+                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
             />
 
         </VStack>
