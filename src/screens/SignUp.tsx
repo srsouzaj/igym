c080eebb8e32ca66cfcdbdbbfb20761e15bcff2b
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-04 18:43:31 -0300

    feat: add react hook form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useForm, Controller } from 'react-hook-form';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
-
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
+
+    const { control } = useForm();
 
     const navigation = useNavigation();
 
@@ -15,11 +17,12 @@ export function SignUp() {
         navigation.goBack();
     }
 
+    function handleSignUp() {
+
+    }
+
     return (
-        <ScrollView
-            contentContainerStyle={{ flexGrow: 1 }}
-            showsVerticalScrollIndicator={false}
-        >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
             <VStack flex={1} px={10} pb={16}>
                 <Image
                     source={BackgroundImg}
@@ -28,36 +31,73 @@ export function SignUp() {
                     resizeMode="contain"
                     position="absolute"
                 />
-
                 <Center my={24}>
                     <LogoSvg />
-
                     <Text color="gray.100" fontSize="sm">
                         Treine sua mente e o seu corpo.
                     </Text>
                 </Center>
-
                 <Center>
                     <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
                         Crie sua conta
                     </Heading>
 
-                    <Input
-                        placeholder="Nome"
+                    <Controller
+                        control={control}
+                        name="name"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Nome"
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder="E-mail"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="E-mail"
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
-                    <Input
-                        placeholder="Senha"
-                        secureTextEntry
+
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Senha"
+                                secureTextEntry
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
-                    <Button title="Criar e acessar" />
+                    <Controller
+                        control={control}
+                        name="password_confirm"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder="Confirmar a Senha"
+                                secureTextEntry
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    />
+
+                    <Button
+                        title="Criar e acessar"
+                        onPress={handleSignUp}
+                    />
                 </Center>
 
                 <Button
