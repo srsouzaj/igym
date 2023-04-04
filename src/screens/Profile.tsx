cc867c5e607c01d21a2b995c19ba2f8923071c45
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-04 17:52:14 -0300

    feat: button update

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,20 +1,17 @@
 import { useState } from 'react';
-import { Center, ScrollView, VStack, Skeleton } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 
-
 const PHOTO_SIZE = 33;
 
 export function Profile() {
-
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
-
     return (
         <VStack flex={1}>
             <ScreenHeader title='Perfil' />
-
             <ScrollView>
                 <Center mt={6} px={10}>
                     {
@@ -28,12 +25,17 @@ export function Profile() {
                             />
                             :
                             <UserPhoto
-                                source={{ uri: 'https://github.com/srsouzaj.png' }}
+                                source={{ uri: 'https://github.com/rodrigorgtic.png' }}
                                 alt="Foto do usuário"
                                 size={PHOTO_SIZE}
                             />
                     }
 
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar Foto
+                        </Text>
+                    </TouchableOpacity>
                 </Center>
             </ScrollView>
         </VStack>
