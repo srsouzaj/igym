bcae25e0ecdd0357bab0cfa303ebe30d6f7bc778
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-04 17:56:11 -0300

    feat: password input

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 
 const PHOTO_SIZE = 33;
+
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
     return (
@@ -37,18 +39,42 @@ export function Profile() {
                             Alterar Foto
                         </Text>
                     </TouchableOpacity>
-
                     <Input
                         bg="gray.600"
                         placeholder='Nome'
                     />
-
                     <Input
                         bg="gray.600"
                         placeholder="E-mail"
                         isDisabled
                     />
                 </Center>
+
+                <VStack px={10} mt={12} mb={9}>
+                    <Heading color="gray.200" fontSize="md" mb={2}>
+                        Alterar senha
+                    </Heading>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Senha antiga"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nova senha"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Confirme a nova senha"
+                        secureTextEntry
+                    />
+
+                    <Button title="Atualizar" mt={4} />
+                </VStack>
             </ScrollView>
         </VStack>
     );
