dfecad8175c611d27865ab0aa676b19332f4b663
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-04 18:06:26 -0300

    fix: exercises image

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,18 +1,18 @@
 import { TouchableOpacity } from 'react-native';
-import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Image, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
-
 import BodySvg from '@assets/body.svg';
 
 export function Exercise() {
-
     const navigation = useNavigation<AppNavigatorRoutesProps>();
+
     function handleGoBack() {
         navigation.goBack();
     }
+
     return (
         <VStack flex={1}>
             <VStack px={8} bg="gray.600" pt={12}>
@@ -24,20 +24,29 @@ export function Exercise() {
                         size={6}
                     />
                 </TouchableOpacity>
-
                 <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
                     <Heading color="gray.100" fontSize="lg" flexShrink={1}>
                         Puxada frontal
                     </Heading>
-
                     <HStack alignItems="center">
                         <BodySvg />
-
                         <Text color="gray.200" ml={1} textTransform="capitalize">
                             Costas
                         </Text>
                     </HStack>
                 </HStack>
+            </VStack>
+
+            <VStack p={8}>
+                <Image
+                    w="full"
+                    h={80}
+                    source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+                    alt="Nome do exercício"
+                    mb={3}
+                    resizeMode="cover"
+                    rounded="lg"
+                />
             </VStack>
         </VStack>
     );
