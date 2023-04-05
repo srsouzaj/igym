e4fa0b2388626fa84fef74ed9d4b5965171bd71a
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-05 10:53:07 -0300

    fix: error validations styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,10 +3,8 @@ import { Input as NativeBaseInput, IInputProps, FormControl } from 'native-base'
 type Props = IInputProps & {
     errorMessage?: string | null;
 }
-
 export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
     const invalid = !!errorMessage || isInvalid;
-
     return (
         <FormControl isInvalid={invalid} mb={4}>
             <NativeBaseInput
@@ -18,6 +16,11 @@ export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
                 color="white"
                 fontFamily="body"
                 placeholderTextColor="gray.300"
+                isInvalid={invalid}
+                _invalid={{
+                    borderWidth: 1,
+                    borderColor: "red.500"
+                }}
                 _focus={{
                     bgColor: 'gray.700',
                     borderWidth: 1,
@@ -25,7 +28,6 @@ export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
                 }}
                 {...rest}
             />
-
             <FormControl.ErrorMessage>
                 {errorMessage}
             </FormControl.ErrorMessage>
