43e2c5c61b8c71567f2de181a5f0824f9767168b
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-05 13:54:27 -0300

    feat: add axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,6 +7,8 @@ import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { api } from "@services/api";
+
 
 type FormDataProps = {
     name: string;
@@ -35,16 +37,8 @@ export function SignUp() {
 
     async function handleSignUp({ name, email, password }: FormDataProps) {
 
-        const response = await fetch('http://192.168.1.101:3333/users', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email, password })
-        })
-        const data = await response.json();
-        console.log(data);
+        const response = await api.post('/users', { name, email, password });
+        console.log(response.data);
     }
 
     return (
