cc87ec08e6a05c4be5dfccbf050a978598939a6f
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-05 18:21:00 -0300

    fix: homeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,19 +2,18 @@ import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Text, VStack, Icon } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import { useAuth } from '@hooks/useAuth';
+
 import { UserPhoto } from './UserPhoto';
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
-import { useNavigation } from '@react-navigation/native';
 
 export function HomeHeader() {
 
-    const navigation = useNavigation<AuthNavigatorRoutesProps>();
-
+    const { user } = useAuth();
 
     return (
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
             <UserPhoto
-                source={{ uri: 'https://github.com/srsouzaj.png' }}
+                source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg}
                 size={16}
                 alt="Imagem do usuário"
                 mr={4}
@@ -24,11 +23,11 @@ export function HomeHeader() {
                 <Text color="gray.100" fontSize="md">
                     Olá,
                 </Text>
-                <Heading color="gray.100" fontSize="md">
-                    Jorge
+
+                <Heading color="gray.100" fontSize="md" fontFamily="heading">
+                    {user.name}
                 </Heading>
             </VStack>
-
 
             <TouchableOpacity>
                 <Icon
@@ -36,7 +35,6 @@ export function HomeHeader() {
                     name="logout"
                     color="gray.200"
                     size={7}
-                    onPress={() => navigation.navigate('signIn')}
                 />
             </TouchableOpacity>
         </HStack>
