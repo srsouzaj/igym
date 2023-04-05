168c2b36b4087981a1e9e6bdb667a6c35ebcfd0f
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-05 15:18:37 -0300

    fix: customHooks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,21 @@
+import { useContext } from 'react';
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useContext } from 'react';
-import { AuthContext } from '@contexts/AuthContext';
+
+import { useAuth } from '@hooks/useAuth';
 
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
     const { colors } = useTheme();
 
-    const contextData = useContext(AuthContext);
+    const { user } = useAuth();
 
-    console.log("USUÁRIO LOGADO =>", contextData);
+    console.log("USUÁRIO LOGADO =>", user);
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
-
     return (
         <Box flex={1} bg="gray.700">
             <NavigationContainer theme={theme}>
