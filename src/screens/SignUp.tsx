36cd155b839ccc52b9122dc57e0fcd84f0c2d1fb
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-05 14:05:49 -0300

    feat: errors api

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,6 @@
 import { Alert } from 'react-native';
 import axios from 'axios';
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 
 import { useForm, Controller } from 'react-hook-form';
@@ -11,12 +11,13 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { api } from "@services/api";
+import { AppError } from '@utils/AppError';
 
 import { signUpSchema } from "@models/schemas/signUp.schema";
 import { FormDataInterface } from "@models/interface/signup.interface";
 
 export function SignUp() {
-
+    const toast = useToast();
     const { control, handleSubmit, formState: { errors } } =
         useForm<FormDataInterface>({
             resolver: yupResolver(signUpSchema),
@@ -34,10 +35,13 @@ export function SignUp() {
             console.log(response.data);
 
         } catch (error) {
-
-            if (axios.isAxiosError(error)) {
-                Alert.alert(error.response?.data.message);
-            }
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
         }
     }
 
