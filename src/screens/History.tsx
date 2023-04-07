0217828c0501286071cdc877457a86620cb4d706
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-07 19:49:50 -0300

    feat: loading history

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
-import { Heading, VStack, SectionList, Text } from 'native-base';
+import { useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
+    const [isLoading, setIsLoading] = useState(true);
     const [exercises, setExercises] = useState([
         {
             title: '26.08.22',
@@ -15,6 +20,36 @@ export function History() {
             data: ["Puxada frontal"]
         }
     ]);
+
+    const toast = useToast();
+
+    async function fetchHistory() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/history');
+
+            console.log(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível carregar os detalhes do exercício';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchHistory()
+        }, [])
+    )
+
     return (
         <VStack flex={1}>
             <ScreenHeader title='Histórico' />
@@ -25,7 +60,7 @@ export function History() {
                     <HistoryCard />
                 )}
                 renderSectionHeader={({ section }) => (
-                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
                         {section.title}
                     </Heading>
                 )}
@@ -39,7 +74,6 @@ export function History() {
                 )}
                 showsVerticalScrollIndicator={false}
             />
-
         </VStack>
     );
 }
