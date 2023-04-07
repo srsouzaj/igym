dcb4a297a3493e3ea65bb6bc8b20aee17f09be99
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-07 19:25:53 -0300

    feat: load image

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -3,18 +3,19 @@ import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base';
 
 import { Entypo } from '@expo/vector-icons';
 
+import { api } from '@services/api';
+
 import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 type Props = TouchableOpacityProps & {
     data: ExerciseDTO;
 };
-
 export function ExerciseCard({ data, ...rest }: Props) {
     return (
         <TouchableOpacity {...rest}>
             <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
                 <Image
-                    source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+                    source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
                     alt="Imagem do exercício"
                     w={16}
                     h={16}
@@ -22,17 +23,14 @@ export function ExerciseCard({ data, ...rest }: Props) {
                     mr={4}
                     resizeMode="center"
                 />
-
                 <VStack flex={1}>
                     <Heading fontSize="lg" color="white" fontFamily="heading">
                         {data.name}
                     </Heading>
-
                     <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
                         {data.series} séries x {data.repetitions} repetições
                     </Text>
                 </VStack>
-
                 <Icon
                     as={Entypo}
                     name="chevron-thin-right"
