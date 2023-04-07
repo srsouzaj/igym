088b1b1d2ae2a817f9f978d2871d8312d6716248
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-07 20:05:28 -0300

    fix: validate password

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -25,8 +25,14 @@ type FormDataProps = {
 }
 
 const profileSchema = yup.object({
-    name: yup.string().required('Informe o nome'),
-    password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos.').nullable().transform((value) => !!value ? value : null),
+    name: yup
+        .string()
+        .required('Informe o nome'),
+    password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 dígitos.')
+        .nullable()
+        .transform((value) => !!value ? value : null),
     confirm_password: yup
         .string()
         .nullable()
@@ -34,7 +40,11 @@ const profileSchema = yup.object({
         .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
         .when('password', {
             is: (Field: any) => Field,
-            then: yup.string().nullable().required('Informe a confirmação da senha.')
+            then: yup
+                .string()
+                .nullable()
+                .required('Informe a confirmação da senha.')
+                .transform((value) => !!value ? value : null)
         }),
 })
 
