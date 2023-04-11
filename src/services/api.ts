5119ba3ac77d28b29cbe3ce8b795d00370a7513d
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-11 17:03:42 -0300

    feat: verify api token valide

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,23 +1,29 @@
 import axios, { AxiosInstance } from "axios";
-
 import { AppError } from "@utils/AppError";
-
 type SignOut = () => void;
-
 type APIInstanceProps = AxiosInstance & {
     registerInterceptTokenManager: (signOut: SignOut) => () => void;
 }
-
 const api = axios.create({
-    baseURL: 'http://192.168.1.101:3333'
+    baseURL: 'http://localhost:3333',
 }) as APIInstanceProps;
 
+
 api.registerInterceptTokenManager = singOut => {
-    const interceptTokenManager = api.interceptors.response.use((response) => response, (error) => {
-        if (error.response && error.response.data) {
-            return Promise.reject(new AppError(error.response.data.message))
+    const interceptTokenManager = api.interceptors.response.use((response) => response, requestError => {
+        if (requestError.response?.status === 401) {
+            if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+
+            }
+
+            singOut();
+
+        }
+
+        if (requestError.response && requestError.response.data) {
+            return Promise.reject(new AppError(requestError.response.data.message))
         } else {
-            return Promise.reject(error)
+            return Promise.reject(requestError)
         }
     });
 
